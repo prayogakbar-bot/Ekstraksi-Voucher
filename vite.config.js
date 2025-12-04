a266d9b6f48f8fced910ceb1b892f9c1d2f832b5
Prayoga Akbar <prayogakbar-bot@users.noreply.github.com>
2025-12-05 03:43:52 +0700

    Fix Vite path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Ekstraksi-Voucher/', // <--- WAJIB
   plugins: [react()],
-  base: '/', // wajib untuk custom domain
 })
